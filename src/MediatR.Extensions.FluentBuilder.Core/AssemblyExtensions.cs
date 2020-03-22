5980825237d75c0655bdd14c97946355e556f9a1
Kaleb Luhman <kaleb.luhman@gmail.com>
2020-03-22 18:52:59 -0500

    adds extension method for loading all request modules as a specifc type

---
+++ src/MediatR.Extensions.FluentBuilder.Core/AssemblyExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace MediatR.Extensions.FluentBuilder
+{
+    public static class AssemblyExtensions
+    {
+        public static IEnumerable<T> GetRequestModulesAs<T>(this Assembly assembly)
+        {
+            return assembly
+                .GetTypes()
+                .Where(x => !x.IsAbstract && x.IsClass && ImplementsRequestModule(x))
+                .Select(Activator.CreateInstance)
+                .Cast<T>();
+        }
+
+        private static bool ImplementsRequestModule(Type type)
+        {
+            foreach (var @interface in type.GetInterfaces())
+            {
+                if (!@interface.IsGenericType)
+                {
+                    continue;
+                }
+
+                if (@interface.GetGenericTypeDefinition() == typeof(IRequestModule<,>))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
