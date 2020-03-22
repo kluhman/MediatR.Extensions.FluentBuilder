665a2892db2b6b79abb877c07a839a1f88fbc46f
Kaleb Luhman <kaleb.luhman@gmail.com>
2020-03-22 18:39:16 -0500

    adds request module interface

---
+++ src/MediatR.Extensions.FluentBuilder.Core/IRequestModule.cs
@@ -0,0 +1,11 @@
+﻿using MediatR.Extensions.FluentBuilder.Builders;
+
+namespace MediatR.Extensions.FluentBuilder
+{
+    public interface IRequestModule<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        // Return type used to enforce pipeline is completely configured
+        // ReSharper disable once UnusedMethodReturnValue.Global
+        IExceptionsPipelineBuilder<TRequest, TResponse> BuildPipeline(IPipelineBuilder<TRequest, TResponse> builder);
+    }
+}
