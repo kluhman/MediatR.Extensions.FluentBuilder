1a511011c6c1ac1f5ac0b08bca15147679f7234f
Kaleb Luhman <kaleb.luhman@gmail.com>
2020-03-22 17:11:00 -0500

    adds initial core builder interfaces

---
+++ src/MediatR.Extensions.FluentBuilder.Core/Builders/IBehaviorBuilder.cs
@@ -0,0 +1,8 @@
+﻿namespace MediatR.Extensions.FluentBuilder.Core.Builders
+{
+    public interface IBehaviorBuilder<out TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        IBehaviorBuilder<TRequest, TResponse> AddBehavior<TBehavior>() where TBehavior : IPipelineBehavior<TRequest, TResponse>;
+        IExceptionsBuilder<TRequest, TResponse> AddHandler<THandler>() where THandler : IRequestHandler<TRequest, TResponse>;
+    }
+}
---
+++ src/MediatR.Extensions.FluentBuilder.Core/Builders/IExceptionsBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MediatR.Pipeline;
+
+namespace MediatR.Extensions.FluentBuilder.Core.Builders
+{
+    public interface IExceptionsBuilder<out TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        IExceptionsBuilder<TRequest, TResponse> AddExceptionHandler<TException, THandler>()
+            where TException : Exception
+            where THandler : IRequestExceptionHandler<TRequest, TResponse, TException>;
+        IExceptionsBuilder<TRequest, TResponse> AddExceptionAction<TException, TAction>()
+            where TException : Exception
+            where TAction : IRequestExceptionAction<TRequest, TException>;
+    }
+}
