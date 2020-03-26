5b7322c368e89bdc9ddb566faf345b2f6745051a
Kaleb Luhman <kaleb.luhman@gmail.com>
2020-03-25 20:14:20 -0500

    adds extension method to add required mediatr classes

--- src/MediatR.Extensions.FluentBuilder.Autofac/ContainerBuilderExtensions.cs
+++ src/MediatR.Extensions.FluentBuilder.Autofac/ContainerBuilderExtensions.cs
@@ -8,6 +8,16 @@ namespace MediatR.Extensions.FluentBuilder
 {
     public static class ContainerBuilderExtensions
     {
+        public static void RegisterMediatR(this ContainerBuilder builder)
+        {
+            builder.RegisterType<Mediator>().As<IMediator>().InstancePerRequest();
+            builder.Register<ServiceFactory>(context =>
+            {
+                var innerContext = context.Resolve<IComponentContext>();
+                return type => innerContext.Resolve(type);
+            });
+        }
+        
         public static void RegisterRequestModules(this ContainerBuilder builder, Assembly assembly)
         {
             foreach (var module in assembly.GetRequestModulesAs<Module>())
--- src/MediatR.Extensions.FluentBuilder.Microsoft/ServiceCollectionExtensions.cs
+++ src/MediatR.Extensions.FluentBuilder.Microsoft/ServiceCollectionExtensions.cs
@@ -8,30 +8,46 @@ namespace MediatR.Extensions.FluentBuilder
 {
     public static class ServiceCollectionExtensions
     {
-        public static void AddModule<TModule>(this IServiceCollection services) where TModule : Module, new()
+        public static IServiceCollection AddModule<TModule>(this IServiceCollection services) where TModule : Module, new()
         {
             services.AddModule(new TModule());
+
+            return services;
         }
         
-        public static void AddModule(this IServiceCollection services, Module module)
+        public static IServiceCollection AddModule(this IServiceCollection services, Module module)
         {
             module.Load(services);
+            
+            return services;
+        }
+
+        public static IServiceCollection AddMediatR(this IServiceCollection services)
+        {
+            services.AddTransient<IMediator, Mediator>();
+            services.AddTransient<ServiceFactory>(p => p.GetService);
+
+            return services;
         }
         
-        public static void AddRequestModules(this IServiceCollection services, Assembly assembly)
+        public static IServiceCollection AddRequestModules(this IServiceCollection services, Assembly assembly)
         {
             foreach (var module in assembly.GetRequestModulesAs<Module>())
             {
                 services.AddModule(module);
             }
+
+            return services;
         }
         
-        public static void AddNotificationModules(this IServiceCollection services, Assembly assembly)
+        public static IServiceCollection AddNotificationModules(this IServiceCollection services, Assembly assembly)
         {
             foreach (var module in assembly.GetNotificationModulesAs<Module>())
             {
                 services.AddModule(module);
             }
+
+            return services;
         }
     }
 }
